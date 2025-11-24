25cb5d38e9d13e32cebfb6c97cd4ec34c56be1f2
Whomerding <wendy.homerding@gmail.com>
2025-11-24 13:41:37 -0500

    html added, basic css added, javascript array created and characters added as objects

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
-alert ("javascript engaged")
+const characterArray = [
+    {
+        number: 0,
+        firstName: "Kimmie",
+        lastName: "Kouture",
+        imageURL: "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExamEzYWJya2dmaDNxMHp3bGZha3RtMDBxMXRvZzU5NXEyYzlua3R2biZlcD12MV9naWZzX3NlYXJjaCZjdD1n/YESRhUJfZtOZG/giphy.gif" ,
+        points: 10,
+        timePopUp: 5,
+    },
+    {
+        number: 1,
+        firstName: "Regina",
+        lastName: "Gorge",
+        imageURL: "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExamEzYWJya2dmaDNxMHp3bGZha3RtMDBxMXRvZzU5NXEyYzlua3R2biZlcD12MV9naWZzX3NlYXJjaCZjdD1n/YESRhUJfZtOZG/giphy.gif" ,
+        points: 10,
+        timePopUp: 5,
+    },
+    {
+        number: 2,
+        firstName: "J.Lo",
+        lastName: "Glow",
+        imageURL: "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExamEzYWJya2dmaDNxMHp3bGZha3RtMDBxMXRvZzU5NXEyYzlua3R2biZlcD12MV9naWZzX3NlYXJjaCZjdD1n/YESRhUJfZtOZG/giphy.gif" ,
+        points: 10,
+        timePopUp: 5,
+    },
+    {
+        number: 3,
+        firstName: "Mariah",
+        lastName: "Magnifique",
+        imageURL: "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExamEzYWJya2dmaDNxMHp3bGZha3RtMDBxMXRvZzU5NXEyYzlua3R2biZlcD12MV9naWZzX3NlYXJjaCZjdD1n/YESRhUJfZtOZG/giphy.gif" ,
+        points: 10,
+        timePopUp: 5,
+    },
+    {
+        number: 4,
+        firstName: "Chloe",
+        lastName: "Kardivva",
+        imageURL: "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExamEzYWJya2dmaDNxMHp3bGZha3RtMDBxMXRvZzU5NXEyYzlua3R2biZlcD12MV9naWZzX3NlYXJjaCZjdD1n/YESRhUJfZtOZG/giphy.gif" ,
+        points: 10,
+        timePopUp: 5,
+    },
+    {
+        number: 5,
+        firstName: "Kenye",
+        lastName: "Next",
+        imageURL: "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExamEzYWJya2dmaDNxMHp3bGZha3RtMDBxMXRvZzU5NXEyYzlua3R2biZlcD12MV9naWZzX3NlYXJjaCZjdD1n/YESRhUJfZtOZG/giphy.gif" ,
+        points: 10,
+        timePopUp: 5,
+    },
+];
+console.log (characterArray[4].number)
+
